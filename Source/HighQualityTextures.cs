569b8d5d296ea32361f227b3312652cac719e392
AUTOMATIC1111 <16777216c@gmail.com>
2019-03-09 01:44:03 +0300

    Initial commit

---
+++ Source/HighQualityTextures.cs
@@ -0,0 +1,18 @@
+﻿using Harmony;
+using RimWorld;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using UnityEngine;
+using Verse;
+
+namespace HighQualityTextures
+{
+    public class HighQualityTextures :Mod
+    {
+        public HighQualityTextures(ModContentPack pack) :base(pack) {
+            var harmony = HarmonyInstance.Create("com.github.automatic1111.highqualitytextures");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }   
+    }
+}
---
+++ Source/PatchModContentLoaderTexture2D.cs
@@ -0,0 +1,33 @@
+﻿using Harmony;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using Verse;
+
+namespace HighQualityTextures
+{
+    [HarmonyPatch(typeof(ModContentLoader<Texture2D>), "LoadPNG", new Type[] { typeof(string) }), StaticConstructorOnStartup]
+    class PatchModContentLoaderTexture2D
+    {
+        static bool Prefix(string filePath, ref Texture2D __result)
+        {
+            Log.Warning("Load: " + filePath);
+
+            Texture2D texture2D = null;
+            if (File.Exists(filePath))
+            {
+                byte[] data = File.ReadAllBytes(filePath);
+                texture2D = new Texture2D(2, 2, TextureFormat.Alpha8, true);
+                texture2D.LoadImage(data);
+                texture2D.name = Path.GetFileNameWithoutExtension(filePath);
+                texture2D.filterMode = FilterMode.Trilinear;
+                texture2D.Apply(true, true);
+            }
+            __result = texture2D;
+            return false;
+        }
+    }
+}
